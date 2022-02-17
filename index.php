0b57c0359917003ea0f8d1ff63e3d503cb783018
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-02-16 17:04:01 -0800

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   
  ?>
   
- <h1><?php echo { $json_data['first-name'] + ' ' + $json_data['last-name']}; ?></h1>
+ <h1><?php echo ($json_data['first-name'] + ' ' + $json_data['last-name']); ?></h1>
  <p> <?php echo $json_data['address'] ?></p>
  <p> <?php echo $json_data['phone']   ?></p>
  
