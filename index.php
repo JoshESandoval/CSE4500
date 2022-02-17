f46db6e1c7b11e544ed0ee73a473342b2421bae0
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-02-16 17:17:29 -0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,13 @@
  $json_data = json_decode($json_file,true);
   
  ?>
+ <head>
   
+  <title>My Resume</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+ </head>
+ 
  <h1><?php echo $json_data['first-name']; echo ' '; echo $json_data['last-name']; ?></h1>
  <p> <?php echo $json_data['address']; ?></p>
  <p> <?php echo $json_data['phone'];   ?></p>
@@ -22,6 +28,7 @@
        <span class = "text-primary"><?php echo $experience['period']; ?></span>
      </div>
   </div>
+}
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 <?php } ?>
