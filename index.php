978886611434c25f48b7c70fb80d93df56ce3e8f
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-02-16 22:36:10 -0800

    Update index.php

--- index.php
+++ index.php
@@ -75,7 +75,7 @@
       <div id="github" class="content-view">
         <div class = "vertical-center">
           <h1>Github Link</h1>
-            <link href="https://github.com/JoshESandoval/CSE4500/edit/main/index.php">Link to Github Repo for class</link>  
+            <div href="https://github.com/JoshESandoval/CSE4500/edit/main/index.php">Link to Github Repo for class</div>  
         </div>
       </div>
     </div>
