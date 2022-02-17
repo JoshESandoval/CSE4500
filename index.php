795e897c547edb7aec32c582d06fba8c5cbf8d20
JoshESandoval <92158813+JoshESandoval@users.noreply.github.com>
2022-02-16 16:52:44 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
   $json_file = file_get_contents('my_data.json');
   $json_data = json_decode($json_file,true);
+  
+  ?>
+<h1><?php echo $json_data['first_name']; ?></h1> 
+  <?php
 
+   
   foreach($json_data['experience'] AS $experience){?>
     <div class = "d-flex flex-column flex-md-row justify-content-between mb-5">
        <div class = "flex-grow-1">
@@ -13,4 +18,6 @@
           <span class = "text-primary"><?php echo $experience['period']; ?></span>
         </div>
     </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
 <?php } ?>
